34504e7229689966e625bb3132830eade44a6b27
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-01-02 19:04:58 +0330

    visualize by default

--- chabok_front/lib/models/server_response.dart
+++ chabok_front/lib/models/server_response.dart
@@ -1,14 +1,25 @@
 import 'dart:convert';
 
+import 'package:chabok_front/services/router.dart';
 import 'package:http/http.dart' as http;
 
 class ServerResponse extends http.Response {
   ServerResponse(super.body, super.statusCode);
 
-  bool get isOk => (super.statusCode / 100).floor() == 2;
+  ServerResponse.visualize(super.body, super.statusCode) {
+    if (isOk || isUnauthorized) return;
+    RouterService.go('/error/$statusCode');
+  }
+
+  bool get isOk => super.statusCode ~/ 100 == 2;
+
+  bool get isServerError => super.statusCode ~/ 100 == 5;
+
+  bool get isRequestError => super.statusCode ~/ 100 == 4;
+
+  bool get isUnauthorized => super.statusCode == 401;
 
   Map<String, dynamic> get bodyJson => jsonDecode(super.body);
 
   String? get message => bodyJson['message'];
-// bodyJson.containsKey('message') ? bodyJson['message'] : null;
 }
