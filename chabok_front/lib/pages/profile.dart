2a1d44265e8385ab759334ce376efa214ff7790e
fatemeh-fo <fotouhi.f20@gmail.com>
2025-01-04 21:52:48 +0330

    make separate modules for user profile sections

--- chabok_front/lib/pages/profile.dart
+++ chabok_front/lib/pages/profile.dart
@@ -1,3 +1,5 @@
+import 'package:chabok_front/models/product.dart';
+import 'package:chabok_front/models/user.dart';
 import 'package:chabok_front/services/router.dart';
 import 'package:chabok_front/widgets/card.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +14,7 @@ class UserProfilePage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 20,
               children: [
                 // User Info Section
                 Card(
@@ -21,134 +24,41 @@ class UserProfilePage extends StatelessWidget {
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage: AssetImage(
-                            'assets/sample_images/seller_pfp.jpg',
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'fff aaa',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black87,
-                              ),
-                            ),
-                            SizedBox(height: 8),
-                            Text(
-                              'Username: fffaaa',
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                            Text(
-                              'Email: fa@example.com',
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                            Text(
-                              'Address: 123 Street',
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                            Text(
-                              'Phone: +1234567890',
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                          ],
-                        ),
-                      ],
+                    child: UserInfoWidget(),
+                  ),
+                ),
+                ProductsListWidget(
+                  title: 'Your Products',
+                  products: List.generate(
+                    5,
+                    (i) => Product(
+                      id: i,
+                      name: 'Product $i',
+                      seller: User(id: i, username: 'ckdks'),
+                      imageUrls: ['assets/sample_images/product_img1.jpg'],
+                      category: '',
+                      location: '',
+                      status: '',
+                      description: 'Description on Product $i',
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
-
-                // Product Section
-                Text(
-                  'Your Products',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blueAccent,
-                  ),
-                ),
-                SizedBox(height: 10),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      margin: EdgeInsets.symmetric(vertical: 8),
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                            'assets/sample_images/product_img1.jpg',
-                          ),
-                        ),
-                        title: Text(
-                          'Product ${index + 1}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text('Description of Product ${index + 1}'),
-                        trailing: Icon(Icons.arrow_forward_ios, size: 18),
-                        onTap: () => RouterService.go(
-                            '/product/$index'), // todo replace with id
-                      ),
-                    );
-                  },
-                ),
-                SizedBox(height: 20),
-                // Purchase Section
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Text(
-                    'Previous Purchases',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blueAccent,
+                ProductsListWidget(
+                  title: 'Previous Purchases',
+                  products: List.generate(
+                    5,
+                    (i) => Product(
+                      id: i,
+                      name: 'Product $i',
+                      seller: User(id: i, username: 'ckdks'),
+                      imageUrls: ['assets/sample_images/product_img1.jpg'],
+                      category: '',
+                      location: '',
+                      status: '',
+                      description: 'Description on Product $i',
                     ),
                   ),
                 ),
-                SizedBox(height: 10),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: 3, // Replace with dynamic purchase count
-                  itemBuilder: (context, index) {
-                    return Card(
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      margin: EdgeInsets.symmetric(vertical: 8),
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                            'assets/sample_images/product_img1.jpg',
-                          ),
-                        ),
-                        title: Text(
-                          'Purchase ${index + 1}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text('Details of Purchase ${index + 1}'),
-                      ),
-                    );
-                  },
-                ),
-                SizedBox(height: 20),
-
                 // Actions Section
                 Text(
                   'Account Actions',
@@ -158,7 +68,6 @@ class UserProfilePage extends StatelessWidget {
                     color: Colors.redAccent,
                   ),
                 ),
-                SizedBox(height: 10),
                 Card(
                   elevation: 3,
                   shape: RoundedRectangleBorder(
@@ -206,5 +115,119 @@ class UserProfilePage extends StatelessWidget {
   void _logout() {
     // todo connect to back
     RouterService.go('/');
+  }
+}
+
+@visibleForTesting
+class UserInfoWidget extends StatelessWidget {
+  const UserInfoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundImage: AssetImage(
+            'assets/sample_images/seller_pfp.jpg',
+          ),
+        ),
+        SizedBox(width: 16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'fff aaa',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Username: fffaaa',
+              style: TextStyle(color: Colors.grey[700]),
+            ),
+            Text(
+              'Email: fa@example.com',
+              style: TextStyle(color: Colors.grey[700]),
+            ),
+            Text(
+              'Address: 123 Street',
+              style: TextStyle(color: Colors.grey[700]),
+            ),
+            Text(
+              'Phone: +1234567890',
+              style: TextStyle(color: Colors.grey[700]),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+@visibleForTesting
+class ProductsListWidget extends StatelessWidget {
+  final String title;
+  final List<Product> products;
+
+  const ProductsListWidget({
+    super.key,
+    required this.title,
+    required this.products,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
+        SizedBox(height: 10),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            final product = products[index];
+
+            return Card(
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              margin: EdgeInsets.symmetric(vertical: 8),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage(product.imageUrls[0]),
+                ),
+                title: Text(
+                  product.name,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  product.description!,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: Icon(Icons.arrow_forward_ios, size: 18),
+                onTap: () => RouterService.go('/product/${product.id}'),
+              ),
+            );
+          },
+        ),
+      ],
+    );
   }
 }
