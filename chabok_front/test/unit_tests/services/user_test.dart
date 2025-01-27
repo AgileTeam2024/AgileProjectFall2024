d4775834020fa8cc177e0765c38687333803fd3b
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-01-27 21:48:52 +0330

    added tests for user_service::get_profile_by_username

--- chabok_front/test/unit_tests/services/user_test.dart
+++ chabok_front/test/unit_tests/services/user_test.dart
@@ -43,6 +43,64 @@ void main() {
     expect(user, isNull);
   });
 
+  test('returns User on successful getProfile fetch', () async {
+    final userJson = {
+      'profile': {'username': 'test_user', 'email': 'test_user@gmail.com'}
+    };
+    when(mockNetworkService.get('/user/get_profile_by_username/test_user'))
+        .thenAnswer((_) async => ServerResponse(jsonEncode(userJson), 200));
+
+    final user = await userService.getProfile('test_user');
+
+    expect(user, isNotNull);
+    expect(user!.username, 'test_user');
+  });
+
+  test('returns null on failed getProfile fetch', () async {
+    when(mockNetworkService.get('/user/get_profile_by_username/test_user'))
+        .thenAnswer((_) async => ServerResponse(jsonEncode({}), 404));
+
+    final user = await userService.getProfile('test_user');
+
+    expect(user, isNull);
+  });
+
+  test('returns User with all fields on successful getProfile fetch', () async {
+    final userJson = {
+      'profile': {
+        'username': 'test_user',
+        'first_name': 'Test',
+        'last_name': 'User',
+        'email': 'test_user@gmail.com',
+        'phone_number': '1234567890',
+        'profile_picture': 'http://example.com/profile.jpg',
+        'address': '123 Test St'
+      }
+    };
+    when(mockNetworkService.get('/user/get_profile_by_username/test_user'))
+        .thenAnswer((_) async => ServerResponse(jsonEncode(userJson), 200));
+
+    final user = await userService.getProfile('test_user');
+
+    expect(user, isNotNull);
+    expect(user!.username, 'test_user');
+    expect(user.firstName, 'Test');
+    expect(user.lastName, 'User');
+    expect(user.email, 'test_user@gmail.com');
+    expect(user.phoneNumber, '1234567890');
+    expect(user.profilePicture, 'http://example.com/profile.jpg');
+    expect(user.address, '123 Test St');
+  });
+
+  test('returns null on getProfile fetch with invalid username', () async {
+    when(mockNetworkService.get('/user/get_profile_by_username/invalid_user'))
+        .thenAnswer((_) async => ServerResponse(jsonEncode({}), 404));
+
+    final user = await userService.getProfile('invalid_user');
+
+    expect(user, isNull);
+  });
+
   test('returns ServerResponse on successful editProfile', () async {
     final fields = {'name': 'new_name'};
     final response = ServerResponse(jsonEncode({}), 200);
